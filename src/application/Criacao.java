76674f24d9c926af26bdb6fa91fa2d3f6b5fd5f1
Evaldo Ribeiro <evardis@gmail.com>
2020-06-15 07:53:11 -0300

    Mensagem de boa noite

--- src/application/Criacao.java
+++ src/application/Criacao.java
@@ -5,7 +5,8 @@ public class Criacao {
 	public static void main(String[] args) {
 		
 		System.out.println("Bom dia, Um ótimo dia dos Namorados!!! (12/06/2020)");
-
+		System.out.println("Boa tarde!!! (15/06/2020)");
+		System.out.println("Boa noite!!! (15/06/2020)");
 	}
 
 }
